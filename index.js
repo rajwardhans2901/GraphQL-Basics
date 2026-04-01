8502213ecbda7284d7feb5ab4aee1c231faf7824
Rajwardhan Sinha <107906822+rajwardhans2901@users.noreply.github.com>
2026-04-01 21:19:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const {url } = await startStandaloneServer(server, {
+    listen : {port:4000}
+})
+
+console.log('Ready')
