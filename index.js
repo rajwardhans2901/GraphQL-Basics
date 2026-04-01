38b10194c244dc12337d5462a71a96d6597920b0
Rajwardhan Sinha <107906822+rajwardhans2901@users.noreply.github.com>
2026-04-01 23:54:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ const resolvers = {
         },
         authors() {
             return db.authors
+        },
+        review(_, args ) {
+            return db.reviews.find((review) => review.id === args.id)
+        },
+        game(_, args ) {
+            return db.games.find((game) => game.id === args.id)
+        },
+        author(_, args ) {
+            return db.authors.find((author) => author.id === args.id)
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r)=> r.game_id === parent.id )
         }
     }
 }
